ad55191a7b7f90d67bee6c2416222c879c94bd10
FitzgeraldChallar <challararise@gmail.com>
2026-01-21 03:55:59 -1200

    Category Page Completed and Banner Re-arranged!

--- cynlink-app/app/(client)/category/[slug]/page.tsx
+++ cynlink-app/app/(client)/category/[slug]/page.tsx
@@ -1,11 +1,27 @@
+import Container from '@/components/container';
+import { Title } from '@/components/ui/text';
+import { getCategories } from '@/sanity/queries'
 import React from 'react'
+import CategoryProducts from '@/components/categoryproducts';
 
-const CategoryPage = () => {
+const CategoryPage = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const categories = await getCategories();
+  const { slug } = await params;
   return (
-    <div>
-      
+    <div className="py-10">
+      <Container>
+        <Title className="text-black text-lg">Products By Categories:{" "}
+          <span className="font-bold text-shop_gold capitalize tracking-wide">{slug && slug}</span>
+        </Title>
+        <CategoryProducts categories={categories} slug={slug} />
+      </Container>
     </div>
   )
 }
 
 export default CategoryPage
+
--- cynlink-app/components/homebanner.tsx
+++ cynlink-app/components/homebanner.tsx
@@ -12,40 +12,40 @@ const HomeBanner = () => {
           bg-banner-bg 
           rounded-lg 
           px-6 md:px-10 lg:px-24 
-          py-10 md:py-0
+          py-10 md:py-5
           flex 
           flex-col md:flex-row 
           items-center 
           md:justify-between
           gap-6 md:gap-0
         ">
-          <div className="text-center md:text-left space-y-5">
-            <Title className="text-shop-charcoal">
-              Get a Deal for Up To 50% off on <br className="hidden sm:block" />
-              Selected Products!
-            </Title>
+          <div className="text-center md:text-left space-y-6 max-w-xl">
 
-            <Link
-              href={"/shop"}
-              className="
-                inline-block
-                mt-4
-                bg-shop_gold/90 
-                text-white/90 
-                px-5 
-                py-2 
-                rounded-md 
-                text-sm 
-                font-semibold 
-                hover:text-white 
-                hover:bg-shop_gold 
-                hoverEffect
-              "
-            >
-              Buy Now
-            </Link>
-          </div>
+  <h1 className="text-white text-2xl sm:text-3xl lg:text-4xl font-extrabold leading-tight tracking-tight">
+    Buy in bulk - wholesale sourcing made SIMPLE! <br />
+    <span className="text-shop_gold">Source safely.</span> <br />
+    Resell with confidence.
+  </h1>
 
+  <Link
+    href={"/shop"}
+    className="
+      inline-block
+      mt-6
+      bg-shop_gold
+      text-white
+      px-6
+      py-3
+      rounded-full
+      text-sm
+      font-semibold
+      hover:bg-shop_gold/90
+      transition-all
+    "
+  >
+    Buy Now
+  </Link>
+</div>
           <div>
             <Image
               src={boy}
